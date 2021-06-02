37b1e32764d5832daaee55054411a16bdd42fd77
Shivbir Kaur <shivbirkaur@amits-air.lan>
2021-06-02 16:24:33 -0400

    add

--- workspacevariables.tf
+++ workspacevariables.tf
@@ -1,24 +1,24 @@
-resource "tfe_organization" "demo" {
-  name  = "demo"
+resource "tfe_organization" "TestingDemo" {
+  name  = "TestingDemo"
   email = "shivbir.kaur@gmail.com"
 }
 
-resource "tfe_workspace" "test" {
+resource "tfe_workspace" "networkmodule" {
   name         = "Network-Module"
-  organization = tfe_organization.demo.id
+  organization = tfe_organization.TestingDemo.id
 }
 
 resource "tfe_variable" "test" {
   key          = "AWS_ACCESS_KEY_ID"
   value        = "AKIA4ZPFYFG3PT5Z377Y"
   category     = "env"
-  workspace_id = tfe_workspace.test.id
+  workspace_id = tfe_workspace.networkmodule.id
   description  = "AWS"
 }
 resource "tfe_variable" "secret" {
   key          = "AWS_SECRET_ACCESS_KEY"
   value        = "GXLWmzT2wjL2S90zy/OCA97MVREmF4lkU+d7iSWY"
   category     = "env"
-  workspace_id = tfe_workspace.test.id
+  workspace_id = tfe_workspace.networkmodule.id
   description  = "AWS"
 }
