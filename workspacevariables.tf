d7b9e63600784bfc0cc1bacba6c1654e49f92569
Shivbir Kaur <shivbirkaur@amits-air.lan>
2021-06-02 15:27:50 -0400

    add

---
+++ workspacevariables.tf
@@ -0,0 +1,17 @@
+resource "tfe_organization" "test" {
+  name  = "shiv-test"
+  email = "shivbir.kaur@gmail.com"
+}
+
+resource "tfe_workspace" "test" {
+  name         = "Network-Module"
+  organization = tfe_organization.test.id
+}
+
+resource "tfe_variable" "test" {
+  key          = "AWS_ACCESS_KEY_ID"
+  value        = "AKIA4ZPFYFG3PT5Z377Y"
+  category     = "env"
+  workspace_id = tfe_workspace.test.id
+  description  = "AWS"
+}
