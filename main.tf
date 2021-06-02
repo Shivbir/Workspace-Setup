4ad2c766aceb02f474391ada08ba122166927af8
Shivbir Kaur <shivbirkaur@amits-air.lan>
2021-06-02 16:02:30 -0400

    hello

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     tfe = {
       version = "~> 0.25.0"
-      token    = "l5eQA9IxLREOiw.atlasv1.sRVKMzDDpx55siDEV3Ib1E0yBxR9wqrt1uuuvIpxVd2AWyLEfStjWml6zuMHvYZ3RBI"
     }
   }
 }
